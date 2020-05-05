2be4f8163de1eb04b1e40186a33d07cecd15c6f0
ncvetic <cuekisa@yahoo.com>
2020-05-06 01:49:11 +0200

    skills icons

--- js/script.js
+++ js/script.js
@@ -1,21 +1,5 @@
-// print
-const printMe = () => {
-    window.print();
-}
-
-const printImg = document.querySelector(".print");
-printImg.addEventListener("click", printMe);
-
 // volume
 document.querySelectorAll("audio").forEach(element => {
     element.volume = 0.4;
 });
 
-// on load
-// if (window.location.href == "https://cvellle.github.io/Nikola-Cvetic-CV/") {
-// }
-
-const all = document.querySelector(".all");
-window.addEventListener("load", () => {
-    all.style.opacity = 1;
-}); window.opacity = 0;
