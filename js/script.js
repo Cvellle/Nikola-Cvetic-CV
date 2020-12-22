59dc4fa01876d1a0945240fe0928a36a1262b941
cvellle <cuekisa@yahoo.com>
2020-12-22 11:05:22 +0100

    root Function

--- js/script.js
+++ js/script.js
@@ -1,12 +1,12 @@
-// volume
-document.querySelectorAll("audio").forEach((element) => {
-  element.volume = 0.4;
-});
-
-// body opacity
 window.addEventListener("load", rootFunction);
 
 function rootFunction() {
+  // body opacity
   document.querySelector("body").style.transition = "2s";
   document.querySelector("body").style.opacity = "1";
+
+  // volume
+  document.querySelectorAll("audio").forEach((element) => {
+    element.volume = 0.1;
+  });
 }
