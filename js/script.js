b6e9219d631650877232e60e94e7e0153b6860a1
cvellle <cuekisa@yahoo.com>
2020-12-22 09:34:09 +0100

    opacity on load

--- js/script.js
+++ js/script.js
@@ -1,5 +1,12 @@
 // volume
-document.querySelectorAll("audio").forEach(element => {
-    element.volume = 0.4;
+document.querySelectorAll("audio").forEach((element) => {
+  element.volume = 0.4;
 });
 
+// body opacity
+window.addEventListener("load", rootFunction);
+
+function rootFunction() {
+  document.querySelector("body").style.transition = "2s";
+  document.querySelector("body").style.opacity = "1";
+}
